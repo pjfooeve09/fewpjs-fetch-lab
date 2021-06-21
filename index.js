b73b0d9111df2bcfb0770830af0b0d17a2e01c66
Parham Javadi <pjfooeve09@yahoo.com>
2021-06-21 14:40:02 -0700

    commit tree de39f11c6d32ec0d1af07042cba9593e2f81c1f9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let books = fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
+  return books;
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
